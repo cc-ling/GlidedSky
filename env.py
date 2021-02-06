6eae38cf785ef01561d4263403afb23ac759c336
cc-ling <36610691+cc-ling@users.noreply.github.com>
2021-02-06 08:53:29 +0800

    Update env.py

--- env.py
+++ env.py
@@ -11,8 +11,8 @@ class env(object):
 	"""docstring for env"""
 	def __init__(self):
 		self.requests = requests.Session()
-		self.email = ''
-		self.password = ''
+		self.email = 账号
+		self.password = 密码
 		self.headers = {
 			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36'
 		}
